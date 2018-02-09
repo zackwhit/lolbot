63d37b5dd923505acf717c6d08b6cb8e73b05ddf
gector <gector@gectors.email>
2018-02-08 23:14:59 -0800

    Added some comments. First stage, will add more later.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/local/bin/python3.6
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
 # This is a discord bot to count how many times people say lol
 import discord
@@ -8,12 +9,18 @@ import sys
 import asyncio
 import json
 
-
 description = "LoL, bot!"
+
+# Path prefix for the lolbot directory containing the count files
 file_loc = "/home/gector/lolbot/"
+
+# Full path for the token file, the file containing the bot token
 token_file="/home/gector/discord_bot_token.txt"
+
+# This gets filled later
 token = ""
 
+# the "lol"s in commands aren't checked.
 commands = [
         "lolcheck",
         "lolremove",
@@ -23,10 +30,12 @@ commands = [
         "lolscore",
         "lolboard"]
 
+# Populate the token file.
 with open(token_file, 'r') as file:
     token = file.readline(100).strip()
 print("Bot token: " + token)
 
+# Init the client
 bot = discord.Client() # commands.Bot(command_prefix='?', description=description)
 
 @bot.event
@@ -36,8 +45,10 @@ async def on_ready():
     print('------------------')
 
 @bot.event
-async def on_message(message):
-    data = 0;
+async def on_message(message): # Basis for most of the bot functions
+    data = 0; # Filled later
+
+    ### ---- Commands {{{{ ---- ###
     if message.content.lower().find("lolcheck") != -1:
         with open(file_loc + "count.txt") as file:
             num = file.readline(9999).strip()
@@ -48,10 +59,12 @@ async def on_message(message):
         await bot.send_message(message.channel, "Restarting: " + message.author.nick);
         #sys.exit()
         raise SystemExit(0)
+
     if message.content.lower().find("lolscore") != -1:
         with open('data.json') as json_data:
             data = json.load(json_data)
-        await bot.send_message(message.channel, message.author.name + ": " + str(data[message.author.name.lower()]))
+        await bot.send_message(message.channel, message.author.name + ": " + 
+                str(data[message.author.name.lower()]))
 
     if message.content.lower().find("lolboard") != -1:
         with open('data.json') as json_data:
@@ -81,10 +94,14 @@ async def on_message(message):
         with open(file_loc + "count.txt", "w") as file:
             file.write(str(num))
         await bot.send_message(message.channel, "\'lol\'s now: " + str(num))
+    
+    ### --- }}}} end commands --- ###
+
 
     if message.content.lower().find("lol") != -1 and message.author.name != bot.user.name:
         print("Got lol")
-
+	
+	# Check for commands the message, if any commands are found then cancel checking for "lol"s
         for x in commands:
             if message.content.lower().find(x) != -1:
                 return;
